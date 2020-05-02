1059bad70c8d6f1bbbb7c6b028ca352a043c33aa
Park chunsung <popobaboya@gmail.com>
2020-05-02 23:39:15 +0900

    Create crud.c and make addProduct() method

---
+++ crud.c
@@ -0,0 +1,11 @@
+int addProduct(Producc *p){
+ getchar();
+ printf("Enter product's name : ");
+ gets(s->name);
+ printf("Enter product's weight : ");
+ scanf("%d", &s->weight);
+ printf("Enter product;s cost");
+ scnaf("%d", %s->cost);
+
+ return 1;
+}
