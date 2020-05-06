2a492197923b2942fbddd8edad376dbe8083d276
ajjang <grace20124@naver.com>
2020-05-06 13:46:44 +0900

    compile with vendingMachine.o

--- main.c
+++ main.c
@@ -1,44 +1,22 @@
-#include <stdio.h>
-#include <string.h>
-
-typedef int bool;
-enum{false, true};
-
-int coint_refill(int *baekwon, int * o_baekwon);
-int change_password(char password[]);
-//int saveData(Product p[], *p);
-//int loadData(Product *p);
-
-void listConsumer();
-void listAdministrator();
+#include "vendingMachine.h"
 
 int main(void){
-	int baekwon=10;
-	int o_baekwon=10;
+	Coin coin;
+	coin.baekwon=10;
+	coin.o_baekwon=10;
+
 	char password[5]="0000";
-	bool consumer= true;
-    int total_won;
-    total_won= (baekwon*100) + (o_baekwon*500);
-	
-	int count;
+	bool consumer= true;	
 	bool exit=false; //if exit is 'true', end the program.
 
 	//count= loadData();
 
 	while(exit==false){
-        printf("password: %s\n",password);
+        	int menu;
+		//printf("password: %s\n",password);
 		while(consumer==true){
-			int menu;
-			printf("\n**Consumer Mode**\n");
-			printf("===============================\n");
-			printf("1. Check Product List\n");
-			printf("2. Buy Product\n");
-			printf("3. Go to Administrator Mode\n");
-			printf("4. Exit\n");
-			printf("===============================\n");
-			printf("Which menu do you choose? (number)>>");
-			scanf("%d",&menu);
-
+			
+			menu= listConsumer();	
 
 			if(menu==1){
 
@@ -50,45 +28,38 @@ int main(void){
 				consumer=false;
 				break;
 			}
-			else if(menu== 4){
+			else if(menu== 0){
 				exit= true;
 				break;
 			}
 
 		}
 		while(consumer==false){
-			int menu;
 			char input_password[4];
+			
 			if(!(strcmp(password,"0000"))){
-				printf("\nWrite password (4 numbers except 0000) >>");
+				
+    				printf("\n===============================\n");
+				printf("Write password (4 numbers except 0000) >>");
 				scanf("%s",password);
 				printf(">>Completed!\n");
+
+    				printf("===============================\n");
 			}
 			else{
-				printf("\nWrite Password >>");
+				
+    				printf("\n===============================\n");
+				printf("Write Password >>");
 				scanf("%s",input_password);
-				if(strcmp(input_password, password)){
+				if(!(strcmp(input_password, password))){
 					printf("**You can't enter Administrator Mode\n");
-                    consumer=true;
-                    break;
+    					printf("===============================\n");
+					consumer=true;
+  					break;
 				}
 
 			}
-			printf("\n**Administrator Mode**\n");
-			printf("===============================\n");
-			printf("1. Check Product List\n");
-			printf("2. Update Product Information\n");
-			printf("3. Delete Product\n");
-			printf("4. Add Prduct\n");
-			printf("5. Search Product\n");
-            printf("6. Check Coin Amount\n");
-            printf("7. Refill Coin\n");
-			printf("8. Go to Consumer Mode\n");
-			printf("9. exit\n");
-			printf("===============================\n");
-			printf("Which menu do you choose? (number)>>");
-			scanf("%d",&menu);
-
+			menu= listAdministrator();
 			if(menu== 1){
 				
 			}
@@ -104,23 +75,28 @@ int main(void){
 			if(menu== 5){
 				
 			}
-            else if(menu==6){
-                printf("\n*Check coin Amount*\n");
-    			printf("===============================\n");
-                printf("Number of 100 won: %d\n",baekwon);
-                printf("Number of 500 won: %d\n",o_baekwon);
-                printf("Total Price: %d\n",total_won);
-    			printf("===============================\n");
+			else if(menu==6){
+				printf("\n*Change Password*\n");
+				changePassword(password);
+			}
+            		else if(menu==7){
+    				coin.total_won= (coin.baekwon*100) + (coin.o_baekwon*500);
+                		printf("\n*Check coin Amount*\n");
+    				printf("===============================\n");
+                		printf("Number of 100 won: %d\n",coin.baekwon);
+                		printf("Number of 500 won: %d\n",coin.o_baekwon);
+                		printf("Total Price: %d\n",coin.total_won);
+    				printf("===============================\n");
 
-            }
-            else if(menu==7){
-
-            }
-			if(menu== 8){
+            		}
+            		else if(menu==8){
+				coinRefill(&coin);
+            		}
+			else if(menu== 9){
 				consumer=true;
 				break;
 			}
-			if(menu== 9){
+			else if(menu== 10){
 				exit=true;
 				break;
 			}
